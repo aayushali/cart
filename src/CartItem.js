e8e47113333c84e2aabe397235abc81ea0eabf2e
aayushali <aayushkhan51@gmail.com>
2021-07-17 10:31:29 +0545

    modified state

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,7 +12,28 @@ class CartItem extends React.Component {
     }
 
     increaseQuantity = () => {
-        console.log('this',this.state);
+       /* This is form 1
+       if we don't require previous state then we use this below component
+       this.setState({
+            qty: this.state.qty + 1
+        })*/
+
+        // Can be written like this too
+        // calling setState basically re renders our component
+        // if we require prev state then we use this below component of setState
+        this.setState((prevState) => {
+          return {  qty: prevState.qty + 1  }
+        });
+    }
+
+    decreaseQuantity = () => {
+        this.setState((prevState)=> {
+            if (prevState.qty > 1) {
+                return {
+                    qty: prevState.qty - 1
+                }
+            }
+        });
     }
     render(){
         const { price, title, qty} = this.state;
@@ -28,7 +49,7 @@ class CartItem extends React.Component {
                 <div className="cart-item-actions">
                     {/*Buttons */}
                     <img src="https://image.flaticon.com/icons/png/512/992/992651.png" alt="increase" className="action-icons" onClick={this.increaseQuantity} />
-                    <img src="https://image.flaticon.com/icons/png/512/992/992683.png" alt="decrease" className="action-icons"/>
+                    <img src="https://image.flaticon.com/icons/png/512/992/992683.png" alt="decrease" className="action-icons" onClick={this.decreaseQuantity} />
                     <img src="https://image.flaticon.com/icons/png/512/1214/1214428.png" alt="delete" className="action-icons"/>
                 </div>
             </div>
